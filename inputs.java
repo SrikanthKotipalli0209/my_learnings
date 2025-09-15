7ebf9f56ebbdd87032fc8de57a88b9dd41663042
srikanthkotipalli0209 <srikanth_kotipalli@apple.com>
2025-09-15 23:29:54 +0530

    Initial commit

---
+++ inputs.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class inputs 
+{
+
+    public static void main(String args[])
+    {
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        int b = scan.nextInt();
+        float c = scan.nextFloat();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+    
+}
